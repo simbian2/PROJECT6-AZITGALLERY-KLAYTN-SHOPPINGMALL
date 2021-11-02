060ead7367f765ce067bdb5d4498eb505022c3c4
simbian2 <simbianartist@gmail.com>
2021-11-02 09:30:33 +0900

    회원가입시 saga 사용해서 post 처리

--- front/client/saga/user.tsx
+++ front/client/saga/user.tsx
@@ -39,9 +39,19 @@ function* SellerAdminSaga(){
     
 }
 
+function SignupAPI(action):any {
+    return axios.post(`http://localhost:4000/user/signup`,JSON.stringify(action.data))
+}
+
+function* SignupSaga(action){
+    const result = yield call(SignupAPI(action))
+
+}
+
 function* watchUser(){
     yield takeLatest('USER_LOGIN_REQUEST',login)
     yield takeLatest('SELLER_ADMIN_SUCCESS',SellerAdminSaga)
+    yield takeLatest('SIGNUP_POST_SUCCESS',SignupSaga)
 }
 
 export default function* userSaga(){
